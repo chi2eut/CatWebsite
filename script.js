02a6b48b6fcb39f8bc7407fd70ab86431504f1f2
chi2eut <chi2eut@gmail.com>
2022-10-15 22:00:22 +0000

    Small Change

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
+document.getElementById("numberSubmit").addEventListener("click", function(event){
+    event.preventDefault();
+    
+    const number = document.getElementById("numberInput").value;
+    
+    if (number < 1) {
+        return;
+    }
+    
+    const url = "https://meowfacts.herokuapp.com/?count=" + number;
+    fetch(url)
+    .then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        let results = "";
+        results += '<div class="catFact">';
+        for (let i = 0; i < json.data.length; i++) {
+            results += '<div class="catFactItem">';
+            results += "<p><strong>Fact#" + (i + 1) + " </strong>" + json.data[i] + "</p>";
+            results += '</div>';
+        }
+        
+        results += '</div>';
+        document.getElementById("catFacts").innerHTML = results;
+    });
+});
 
+document.getElementById("picNumSubmit").addEventListener("click", function(event){
+    event.preventDefault();
+    const number = document.getElementById("picNumInput").value;
+    
+    if (number < 1) {
+        return;
+    }
+    
+    const url = 'https://shibe.online/api/cats?count=' + number + '&urls=true&httpsUrls=true';
+    fetch(url)
+    .then(function(response) {
+        return response.json();
+    }).then(function(json) {	
+        let result = "";
+        result += '<div class="catPicture">';
+        for (let i = 0; i < json.length; i++) {
+            result += '<div class="catPictureItem">';
+            result += '<img src="' + json[i] + '" height=400 />';
+            result += '</div>';
+        }
+        
+        result += '</div>';
+        document.getElementById("catPictures").innerHTML = result;
+    });
+});
